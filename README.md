ef5554efe052966d733ddd03422020675161c85a
lcc2207 <lynn@fidmail.com>
2018-04-26 13:25:41 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-client-base
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef-client-base
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
